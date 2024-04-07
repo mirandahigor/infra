15ffd850872d7145d5a9f6e1f4100fc9aed1b579
mirandahigor <81437236+mirandahigor@users.noreply.github.com>
2024-04-07 20:56:27 -0300

    subindo ecs cluster

--- .github/workflows/terraform/main.tf
+++ .github/workflows/terraform/main.tf
@@ -11,11 +11,13 @@ resource "aws_ecs_service" "my_service" {
   cluster         = aws_ecs_cluster.my_cluster.id
   task_definition = aws_ecs_task_definition.my_task_definition.arn
   desired_count   = 1
+  launch_type     = "FARGATE"
   depends_on      = [aws_ecs_task_definition.my_task_definition]
 
   network_configuration {
     subnets          = [aws_subnet.my_subnet.id]
     security_groups  = [aws_security_group.my_security_group.id]
+    assign_public_ip = true
   }
 
   load_balancer {
@@ -27,6 +29,12 @@ resource "aws_ecs_service" "my_service" {
 
 resource "aws_ecs_task_definition" "my_task_definition" {
   family                = "my_task_definition"
+  network_mode          = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                   = "256"
+  memory                = "512"
+  execution_role_arn    = aws_iam_role.ecs_task_execution_role.arn
+
   container_definitions = jsonencode([{
     name      = "my_container"
     image     = "nginx:latest"
@@ -34,8 +42,21 @@ resource "aws_ecs_task_definition" "my_task_definition" {
     memory    = 512
     portMappings = [{
       containerPort = 80
-    }
-    ]
-    }])     
+      hostPort      = 80
+      protocol      = "tcp"
+    }]
+  }])
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
